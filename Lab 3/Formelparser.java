661fc4b0f9faa06a0924715fdbec49eb31fec6af
Nicole <66645928+nicoleno@users.noreply.github.com>
2022-11-02 13:18:16 +0100

    isNumber, isOperator

--- Lab 3/Formelparser.java
+++ Lab 3/Formelparser.java
@@ -3,15 +3,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Formelparser {
-    public static String checkOperator(String operator) {
-      
+    public static boolean isOperator(String operator) {
+        // <Operator> ::= + | - | *
         if (operator == "+" || operator == "-" || operator == "*" ){
-            return operator;
+            return true;
         }
         else{
-            return "";
+            return false;
         }
+    }
 
+    public static boolean isNumber(String strNum) { //remake
+        // <Siffra> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isExpression(String expr){
+        // <Uttryck> ::= <Tal> | "(" <Formel> ")"
+        if (expr == null){
+            return false;
+        }
+        if (isNumber(expr) == true){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
@@ -26,12 +51,8 @@ public class Formelparser {
             numberofChars += 1;
             String str = String.valueOf(ch);
             characters.add(str);
-
         }
-
-        System.out.println(checkOperator("2"));
-        System.out.println(checkOperator("*"));
-
+        System.out.println(isExpression("23424"));     
     }
 }
 // javac Words.java
