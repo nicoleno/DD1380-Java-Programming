5ae33ab7d90d0a053744538889db33a367e83eef
Nicole <66645928+nicoleno@users.noreply.github.com>
2022-11-03 00:17:01 +0100

    progress or not

--- Lab 3/Formelparser.java
+++ Lab 3/Formelparser.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 public class Formelparser {
-    public static boolean isOperator(String operator) {
+    public static String isOperator(String operator) {
         // <Operator> ::= + | - | *
         if (operator == "+" || operator == "-" || operator == "*" ){
-            return true;
+            return operator;
         }
         else{
-            return false;
+            return "false";
         }
     }
 
     public static boolean isNumber(String strNum) { //remake
         // <Siffra> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+    
         if (strNum == null) {
             return false;
         }
@@ -25,6 +27,7 @@ public class Formelparser {
         }
         return true;
     }
+
 
     public static boolean isExpression(String expr){
         // <Uttryck> ::= <Tal> | "(" <Formel> ")"
@@ -42,17 +45,31 @@ public class Formelparser {
     public static void main(String[] args) {
 
         List<String> characters = new ArrayList<String>();
+        LinkedList<String> list = new LinkedList();
         Scanner sc = new Scanner(System.in);
         String words = sc.nextLine();
         int numberofChars = 0;
+        int listSize = 0;
         words = words.replace(" ", "");
 
         for (char ch: words.toCharArray()) {
             numberofChars += 1;
             String str = String.valueOf(ch);
             characters.add(str);
+            list.add(str);
+            listSize += 1;
         }
-        System.out.println(isExpression("23424"));     
+     
+        for (int i=0; i < listSize; i++){
+            String currentSymbol = list.peek();
+            if (isNumber(currentSymbol)){
+                String siffra1 = currentSymbol;
+            }
+            if (isExpression(currentSymbol)){
+                String expr = currentSymbol;
+            }
+        }
+        
     }
 }
 // javac Words.java
